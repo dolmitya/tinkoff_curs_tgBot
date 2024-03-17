edba287a0429139de730f926fcbee5b547b42998
dolmitya <146749982+dolmitya@users.noreply.github.com>
2024-03-17 21:58:57 +0300

    Revert "hw1"

--- bot/src/main/java/edu/java/bot/BotApplication.java
+++ bot/src/main/java/edu/java/bot/BotApplication.java
@@ -1,8 +1,6 @@
-package edu.java.bot.configuration;
+package edu.java.bot;
 
-import edu.java.bot.controller.TelegramBot;
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
+import edu.java.bot.configuration.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -10,17 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties(ApplicationConfig.class)
 public class BotApplication {
-
-    @Autowired
-    private ApplicationConfig applicationConfig;
-
     public static void main(String[] args) {
         SpringApplication.run(BotApplication.class, args);
-    }
-
-    @PostConstruct
-    private void runBot() {
-        TelegramBot bot = new TelegramBot(applicationConfig.telegramToken());
-        bot.run();
     }
 }
--- bot/src/main/java/edu/java/bot/users/State.java
+++
@@ -1,7 +0,0 @@
-package edu.java.bot.users;
-
-public enum State {
-    NONE,
-    ADD_LINK,
-    DEL_LINK;
-}
